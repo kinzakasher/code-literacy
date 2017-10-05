bdef0119f131412fc7270b93be9bfcd0db3b9a59
kinzakasher <32021945+kinzakasher@users.noreply.github.com>
2017-10-05 19:05:57 -0400

    Add files via upload

---
+++ object-sample.py
@@ -0,0 +1,12 @@
+{
+	"make": "Ford", 
+	"model": "Explorer XLT", 
+	"year": "2005", 
+	"color": "tan",
+	"mileage": "105000",
+	"bodytype": "SUV",
+	"engine": "6Cyl",
+
+}
+
+# description of my car
