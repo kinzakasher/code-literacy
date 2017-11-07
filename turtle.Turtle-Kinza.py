a8e6891e040d8e6387d4b4cd44a71b417cbbc004
kinzakasher <32021945+kinzakasher@users.noreply.github.com>
2017-11-06 22:18:31 -0500

    Add files via upload

---
+++ turtle.Turtle-Kinza.py
@@ -0,0 +1,148 @@
+import turtle
+
+turtle.penup()
+for i in range(1, 100, 25):
+    turtle.right(90)    # Face south
+    turtle.forward(i)   # Move radius
+    turtle.right(270)   # Back to start 
+    turtle.pendown()    # Put pen down
+    turtle.circle(i)    # Draw a circle
+    turtle.penup()      # Pen up
+    turtle.home()       # Head back to start
+    turtle.speed(0)     # Speed up turtle
+    
+    
+for i in ['red', 'green', 'yellow', 'pink', 'orange']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(100)
+    turtle.pendown()
+
+for i in range(250): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+    
+for i in ['red', 'green', 'yellow', 'blue', 'pink']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.left(300)
+    turtle.pendown()
+
+for i in range(250): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+    
+for i in ['red', 'green', 'yellow', 'pink', 'green']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(300)
+    turtle.pendown()
+
+for i in range(100): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+    
+for i in ['red', 'blue', 'yellow', 'pink', 'yellow']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(100)
+    turtle.pendown()
+
+for i in range(250): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+
+for i in ['red', 'green', 'yellow', 'pink', 'blue']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(100)
+    turtle.pendown()
+
+for i in range(80): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+
+for i in ['red', 'green', 'yellow', 'pink', 'violet']:
+    turtle.color(i)
+    turtle.right(200)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(100)
+    turtle.pendown()
+
+for i in range(100): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+    
+for i in ['red', 'green', 'yellow', 'pink', 'orange']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(100)
+    turtle.pendown()
+
+for i in range(250): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+    
+for i in ['red', 'green', 'yellow', 'pink', 'red']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(100)
+    turtle.pendown()
+
+for i in range(250): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+
+for i in ['red', 'green', 'yellow', 'pink', 'indigo']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(300)
+    turtle.pendown()
+
+for i in range(250): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+    
+    turtle.penup()
+    turtle.back(400)
+    turtle.pendown()
+
+for i in ['red', 'green', 'yellow', 'pink', 'indigo']:
+    turtle.color(i)
+    turtle.forward(75)
+    turtle.left(90)
+
+    turtle.penup()
+    turtle.forward(300)
+    turtle.pendown()
+
+for i in range(250): # this "for" loop will repeat these functions 250 times
+    turtle.forward(i)
+    turtle.left(91)
+    
